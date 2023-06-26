9e340e76b980b1b5538ec9814f55a644e5e9fc35
Meysam Kia <msmkia@outlook.com>
2023-06-26 19:26:30 +0330

    continuing whith integrating gravity data

--- src/blocks/dashboard/render.php
+++ src/blocks/dashboard/render.php
@@ -4,6 +4,58 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+function adeptivity_blocks_entries_by_user()
+{
+  $table_id = '1';
+  $cu_user_id = wp_get_current_user()->ID;
+  $entries = GFAPI::get_entries($table_id);
+  $field_id = 'created_by';
+  $filtered_entries = array_filter($entries, function ($entry) use ($field_id, $cu_user_id) {
+    return rgar($entry, $field_id) == $cu_user_id;
+  });
+
+  return $filtered_entries;
+}
+
+function adeptivity_blocks_entries_for_dashboard()
+{
+  return array_map(function ($e) {
+    return [
+      'created_at' => $e['date_created'],
+      'lecture_name' => $e['3'],
+      'course_name' => $e['4'],
+      'divergent' => $e['10'],
+      'lateral' => $e['14'],
+      'aesthetic' => $e['13'],
+      'system' => $e['11'],
+      'inspirational' => $e['12'],
+    ];
+  }, adeptivity_blocks_entries_by_user());
+}
+
+function adeptivity_blocks_score_history_by_cat($cat)
+{
+  return array_reduce(
+    adeptivity_blocks_entries_for_dashboard(),
+    function ($carry, $item) use ($cat) {
+      array_push($carry, $item[$cat]);
+      return $carry;
+    },
+    []
+  );
+}
+
+function adeptivity_blocks_max_scores()
+{
+  return [
+    'divergent' => max(adeptivity_blocks_score_history_by_cat('divergent')),
+    'lateral' => max(adeptivity_blocks_score_history_by_cat('lateral')),
+    'aesthetic' => max(adeptivity_blocks_score_history_by_cat('aesthetic')),
+    'system' => max(adeptivity_blocks_score_history_by_cat('system')),
+    'inspirational' => max(adeptivity_blocks_score_history_by_cat('inspirational')),
+  ];
+}
+
 function gravity_forms_entries_shortcode()
 {
 
@@ -29,11 +81,11 @@ function gravity_forms_entries_shortcode()
   });
   ?>
   <pre>
-      <?php
-      var_dump($entries);
-      wp_die();
-      ?>
-    </pre>
+          <?php
+          var_dump($entries);
+          wp_die();
+          ?>
+        </pre>
   <?php
   // $filtered_entries = intval($filtered_entries);
   //print_r ($filtered_entries);
@@ -95,12 +147,23 @@ function get_max_values($two_dim_array)
 
   return $max_values;
 }
-
-$scores = gravity_forms_entries_shortcode();
+?>
+<pre>
+<<?php
+var_dump(adeptivity_blocks_max_scores());
+wp_die();
+?>
+</pre>
+<?php
+// $scores = gravity_forms_entries_shortcode();
 
 
 $basename = parse_url(home_url())['path'] ?? '';
 ?>
-<div id="root" <?php echo get_block_wrapper_attributes(); ?> data-assetdir="<?php echo esc_attr(ASSETDIR) ?>"
-  data-basename="<?php echo esc_url($basename) ?>">
+<div 
+  id="root"
+  <?php echo get_block_wrapper_attributes(); ?>
+  data-assetdir="<?php echo esc_attr(ASSETDIR) ?>"
+  data-basename="<?php echo esc_url($basename) ?>"
+>
 </div>
