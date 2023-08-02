4346815c6753762cc16225e7a7d2aeb3e928ff2c
Meysam Kia <msmkia@outlook.com>
2023-08-02 17:56:17 +0330

    add info for each create profile page

--- src/blocks/createprofile/constants.js
+++ src/blocks/createprofile/constants.js
@@ -9,3 +9,76 @@ export const QUERIES = {
   tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
   laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
 };
+
+export const pageInfo = {
+  school: {
+    avatarPath: './svg/school',
+    field1: {
+      type: 'text',
+      name: 'name',
+      label: 'Name',
+    },
+    field2: {
+      type: 'number',
+      name: 'stucount',
+      label: 'Student Count',
+    },
+    field3: {
+      type: 'date',
+      name: 'dateofbirth',
+      label: 'Date of Birth',
+    },
+    field4: {
+      type: 'tel',
+      name: 'phone',
+      label: 'Phone Number',
+    },
+    field9: {
+      type: 'text',
+      name: 'school',
+      label: 'School',
+    },
+    field10: {
+      type: 'text',
+      name: 'job',
+      label: 'Job Title',
+    },
+  },
+  teacher: {
+    avatarPath: './svg/teacher',
+    field1: {
+      type: 'text',
+      name: 'name',
+      label: 'First Name',
+    },
+    field2: {
+      type: 'text',
+      name: 'family',
+      label: 'Last Name',
+    },
+    field3: {
+      type: 'date',
+      name: 'estdate',
+      label: 'Establishment Date',
+      placeHolder: 'Establishment Date'
+    },
+    field4: {
+      type: 'tel',
+      name: 'phone',
+      label: 'Phone / Fax',
+      placeHolder: 'Phone / Fax'
+    },
+    field9: {
+      type: 'text',
+      name: 'principal',
+      label: 'Principal Name',
+      placeHolder: 'Principal Name'
+    },
+    field10: {
+      type: 'text',
+      name: 'principalphone',
+      label: 'Principal Phone no',
+      placeHolder: 'Principal Phone no.'
+    },
+  }
+}
